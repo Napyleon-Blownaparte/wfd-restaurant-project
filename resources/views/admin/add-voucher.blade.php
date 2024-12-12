2abb6f87e219a2a2e90bd639608143da43e15c94
Lea <c14220168@john.petra.ac.id>
2024-12-12 18:13:55 +0700

    add vouch (base)

---
+++ resources/views/admin/add-voucher.blade.php
@@ -0,0 +1,30 @@
+<x-app-layout>
+    <div class="flex mt-16">
+        <!-- Sidebar -->
+        
+
+        <!-- Main Content -->
+        <div class="flex-1 ml-64 p-8 bg-gray-100">
+            <div class="flex justify-between items-center mb-6">
+                <h1 class="text-2xl font-bold">Category A</h1>
+                <button class="px-4 py-2 bg-amber-400 text-black hover:bg-amber-500 rounded">Add Menu</button>
+            </div>
+            <div class="grid grid-cols-3 gap-6">
+                <!-- Card 1 -->
+                
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
+
+<!-- <style>
+    .scrollbar-hide::-webkit-scrollbar {
+        display: none;
+    }
+    
+    .scrollbar-hide {
+        -ms-overflow-style: none; 
+        scrollbar-width: none;  
+    }
+    </style> -->
